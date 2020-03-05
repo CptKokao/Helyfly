7d1351341c265c482766c1bce194835da88bc5f5
Markov Sergey <markov@qtech.ru>
2020-03-05 18:19:18 +0300

    подготовил графику, шрифты

---
+++ source/js/script.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+'use strict';
+
+// polyfill swiper for IE11
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+      value: function(search, rawPos) {
+          var pos = rawPos > 0 ? rawPos|0 : 0;
+          return this.substring(pos, pos + search.length) === search;
+      }
+  });
+}
+
+/* Слайдер для блока tab */
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+  },  
+  // autoplay: {
+  //   delay: 4000,
+  // },
+  loop: true,
+});
+
+/* Слайдер для блока slider */
+var mySwiper = new Swiper('.swiper-container-tab', {
+  pagination: {
+    el: '.swiper-pagination-tab',
+  },  
+  // autoplay: {
+  //   delay: 4000,
+  // },
+  loop: true,
+});
