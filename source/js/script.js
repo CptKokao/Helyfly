ea26887f57fbfaa3a7bb95c39d7cae024f1f1850
CptKokao <pro-markov@yandex.ru>
2020-03-08 20:44:02 +0300

    добавляет yandex map

--- source/js/script.js
+++ source/js/script.js
@@ -17,3 +17,33 @@ var mySwiper = new Swiper('.swiper-container', {
   spaceBetween: 20,
   loop: true,
 });
+
+/* Yandex карта */
+
+ymaps.ready(init);
+function init(){
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [55.75897861, 37.61587440],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 13,
+      controls: ['zoomControl'],
+      behaviors: ['drag']
+  });
+
+  var myPlacemark = new ymaps.Placemark([55.75897861, 37.61587440], 
+    {balloonContent: 'Встречаемся тут!'}, 
+    {preset: "islands#redIcon"}
+  );
+
+  // myPin.add(myPlacemark);
+  myMap.geoObjects.add(myPlacemark);
+
+  // Балун откроется в точке «привязки» балуна — т. е. над меткой.
+  // myMap.balloon.open();
+};
