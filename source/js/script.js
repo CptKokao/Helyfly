359f9c5c7902ab754a909c35749d623fc2510c9c
CptKokao <pro-markov@yandex.ru>
2020-03-10 08:01:56 +0300

    добавляет код для ссылок more

--- source/js/script.js
+++ source/js/script.js
@@ -110,3 +110,99 @@ $("#date").mask("99.99.9999");
 $("#card").mask("99999-9999-9999-9999");
 $("#date-card").mask("99.9999");
 $("#cvv").mask("999");
+
+
+/* видео youtube */
+let video = document.querySelector('.desc__video');
+let videoImg = document.querySelector('.desc__video-img');
+
+video.addEventListener('click', () => {
+  let iframe = createIframe();
+
+  videoImg.remove();
+  video.appendChild(iframe);
+});
+
+/* cоздаем iframe */
+function createIframe() {
+  let iframe = document.createElement('iframe');
+  
+	iframe.setAttribute('src', 'https://www.youtube.com/embed/4JS70KB9GS0?autoplay=1');
+	iframe.setAttribute('allowfullscreen', '');
+	iframe.setAttribute('allow', 'autoplay');
+
+	return iframe;
+}
+
+
+/* cоздаем линк еще для блока Отзывы */
+let linkMore = document.querySelector('.review__link');
+let block = document.querySelector('.review__list');
+
+linkMore.addEventListener('click', (e) => {
+  e.preventDefault();
+  block.insertAdjacentHTML('beforeend', 
+    `
+    <!-- Отзыв -->
+    <div class="review__item">
+
+      <div class="review__img">
+        <img src="./img/antonova.png" alt="Валерия Антонова">
+      </div>
+
+      <p class="review__title-name">Валерия Антонова</p>
+      <p class="review__title-prof">Арт-директор</p>
+      <div class="review__title-wrap">
+        <div class="review__title-rating">
+          <img src="./img/rating.svg" alt="Рейтинг">
+        </div>
+        <p class="review__extra">Супер! Я бы заказала съемку снова!</p>
+      </div>
+
+      <p class="review__text">Хотим выразить огромную благодарность вашей компании! Получили море ощущений, все прошло просто замечательно. Фотки получились великолепные, отдельное спасибо фотографу!</p>
+      <p class="review__data">27 Апреля, 2019г.</p>
+    </div>
+    `
+  );
+});
+
+/* cоздает линк еще для блока about__leader - Отзывы */
+let aboutLeaderLink = document.querySelector('.about__leader-link');
+let aboutLeader = document.querySelector('.about__leader');
+
+aboutLeaderLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutLeader.insertAdjacentHTML('beforeend', 
+    `
+    <p>Сначала мы (я и моя команда) изучаем интересы клиентов, а затем придумываем подходящее решение, которое соответствует бюджету и планам клиента. Мы никогда не зависим от субпоставщиков и не свяжемся.</p>
+    `
+  );
+  aboutLeaderLink.remove();
+});
+
+/* cоздает линк еще для блока about__services - Отзывы */
+let aboutServices = document.querySelector('.about__services');
+let aboutServicesLink = document.querySelector('.about__services-link');
+
+aboutServicesLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  aboutServices.insertAdjacentHTML('beforeend', 
+    `
+    <p>Качественный подбор места и локации для вашей съемки. При необходимости всегда возможно прерваться на обед/ужин. Более 50 фотографий на выходе с обработкой и замечательные впечатления о Дубае !</p>
+    `
+  );
+  aboutServicesLink.remove();
+});
+
+/* cоздает дополнительные фото для юлока gallery */
+let galleryWrap = document.querySelector('.gallery__wrap');
+let galleryMore = document.querySelector('.gallery__more');
+
+
+galleryMore.addEventListener('click', (e) => {
+  e.preventDefault();
+  let galleryWrapClone = galleryWrap.cloneNode(true); // клонировать сообщение
+  console.log(galleryWrapClone)
+  galleryWrap.insertAdjacentElement('beforeend', galleryWrapClone);
+  galleryMore.remove();
+});
